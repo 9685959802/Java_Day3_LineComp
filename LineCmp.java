0a3d4fcddce002584b9ed8e601f28b51147cf6bc
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-06 10:52:54 +0530

    Line based on a point consisting x,y co-ordinates

--- LineCmp.java
+++ LineCmp.java
@@ -1,8 +1,35 @@
 package Brigdelabz.java.linecomp;
 
+import java.util.Scanner;
+
 public class LineCmp {
 	 //Master Branch
 	public static void main(String []args) {
 	System.out.println("Welcome to Line Comparison Computation Program");
+	//UC1-model a line based on a point consisting of (x, y) co-ordinates using the Cartesian system, So that I can calculate its length.
+    int x1, x2, y1, y2;
+    double length;
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter the end points of line ");
+    
+    //Enter end points of line 1
+
+	System.out.println("Enter x1 : ");
+	x1 = sc.nextInt();
+	System.out.println("Enter y1 : ");
+	y1 = sc.nextInt();
+	
+	//Enter end points of line 2
+	
+	System.out.println("Enter x2 : ");
+	x2 = sc.nextInt();
+	System.out.println("Enter y2 : ");
+	y2 = sc.nextInt();
+	length = Math.sqrt((Math.pow((x2-x1), 2)+Math.pow((y2-y1), 2)));
+	System.out.println("Length of the Line is : " + length);
+    
+
 	}
+	
 }
