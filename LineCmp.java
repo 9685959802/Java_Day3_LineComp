3622ae36e2e3bbd20aa76b01f403e62fc3759be4
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-06 09:51:32 +0530

    Welcome to line comparision Computation Program

---
+++ LineCmp.java
@@ -0,0 +1,8 @@
+package Brigdelabz.java.linecomp;
+
+public class LineCmp {
+	 //Master Branch
+	public static void main(String []args) {
+	System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
