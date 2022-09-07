3a4cf5a7737003cfbae07212cb5b1363595b6cda
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-07 15:59:14 +0530

    Check equality of two lines

--- LineCmp.java
+++ LineCmp.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class LineCmp {
 	 //Master Branch
-	public static void main(String []args) {
+	public static void main(String []args, String length1, String length2) {
 	System.out.println("Welcome to Line Comparison Computation Program");
 	//UC1-model a line based on a point consisting of (x, y) co-ordinates using the Cartesian system, So that I can calculate its length.
     int x1, x2, y1, y2;
@@ -28,8 +28,17 @@ public class LineCmp {
 	y2 = sc.nextInt();
 	length = Math.sqrt((Math.pow((x2-x1), 2)+Math.pow((y2-y1), 2)));
 	System.out.println("Length of the Line is : " + length);
+	
+	//UC2-check equality of two lines based on the end points, whether two lines are the equal or not
+	Double line1 = new Double(length1);
+	Double line2 = new Double(length2);
+	if (line1.equals(line2))
+        System.out.println("Line 1 and line 2 have Equal lengths.");
+    else
+        System.out.println("Line 1 and Line 2 do not have Equal lengths.");
+	}
+}
+
     
 
-	}
 	
-}
